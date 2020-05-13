a29f42a7bfb2658c652bcf7c43734cf03d1523a7
NielsGregers <niels@jumpto365.com>
2020-05-13 20:11:11 +0200

    Updates

---
+++ precache-manifest.0b3199907374c78d2a5bcee71dea3d25.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3e98b06637b358f0819dce4e5e9c7387",
+    "url": "/index.html"
+  },
+  {
+    "revision": "98b8c368126c241194ae",
+    "url": "/static/css/main.75d6fcaa.chunk.css"
+  },
+  {
+    "revision": "c59738e1bdbbffbfa57a",
+    "url": "/static/js/2.141e61ee.chunk.js"
+  },
+  {
+    "revision": "98b8c368126c241194ae",
+    "url": "/static/js/main.33d16114.chunk.js"
+  },
+  {
+    "revision": "0df1c3491836d6324e7f",
+    "url": "/static/js/runtime-main.89f86809.js"
+  }
+]);
